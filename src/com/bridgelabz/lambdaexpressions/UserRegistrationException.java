9bb2b8e96b924b1ed41cf9f9831ecf0d7e26fe8b
Ameet Jadhav <ammugs11@gmail.com>
2022-09-30 17:49:21 +0530

    added using lambda function code to user entry

--- src/com/bridgelabz/lambdaexpressions/UserRegistrationException.java
+++ src/com/bridgelabz/lambdaexpressions/UserRegistrationException.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.lambdaexpressions;
 
-public class UserRegistrationException {
+public class UserRegistrationException extends Exception {
+
+	public UserRegistrationException(String string) {
+		// TODO Auto-generated constructor stub
+	} 
 
 }
